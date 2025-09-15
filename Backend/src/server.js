0de8bffafb360edc1d9c7636cb99142274c2ff51
dgmp755 <dgmp755@gmail.com>
2025-09-16 00:36:59 +0530

    under dev

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,5 +1,9 @@
 import express from "express";
 import notesRoute from "./routes/notesRoute.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+connectDB();
 const app = express();
 app.listen(5001,() =>{
     console.log("Server started");
